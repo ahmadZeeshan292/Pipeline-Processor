429108ab2f4b06ca936b9950d0fc61856d3dd4b6
ahmadZeeshan292 <itzahmad292@gmail.com>
2025-04-15 22:49:47 +0500

    Update Fetch_register.v

--- Fetch_register.v
+++ Fetch_register.v
@@ -16,4 +16,37 @@ module Fetch_register(
 // J type instruction            //                                           |
   output reg [25:0] imm,         //                                           |
   output reg [31:0] PC_next_IF
-);
+);                               //                                           |
+                                 //                                           |  
+always @(posedge clk)            //                                           |
+begin    
+if(register_write == 0)  begin                                                   
+// common data between all instructions
+ opcode <=  {1'b0,instruction [31 :26]};   // Note: in opcode we can concatinating the opcode bits of instruction with 0
+// common data between R and I type
+ rs1<= instruction [25 :21];
+ rs2 <= instruction [20 :16];
+// R type instruction
+ rd <= instruction[15:11];
+ shamt <= instruction[10:6];
+ funct <= instruction[5:0];
+// I type instruction
+ offset <= instruction[15:0];
+// J type instruction
+ imm <= instruction[25:0];
+ PC_next_IF <= PC_next;
+end
+if(IF_Flush) begin
+ opcode <=  7'd50;   
+ rs1<= 5'd0;
+ rs2 <= 5'd0;
+ rd <= 5'd0;
+ shamt <= 5'd0;
+ funct <= 6'd0;
+ offset <= 16'd0;
+ imm <= 26'd0;
+ PC_next_IF <= 32'd0;
+end
+  end
+
+endmodule
